8bc9a577042f84a3ef2ca7d786bf53d975f965b3
Sam Hocevar <sam@zoy.org>
2005-03-14 22:30:41 +0000

    * oops, fixed a stupid issue with my last end message commit, thanks Olivier.
    git-svn-id: svn://svn.zoy.org/monsterz@89 5a8f7f75-4cf0-0310-8b82-9b2b8c614a87

--- monsterz.py
+++ monsterz.py
@@ -570,11 +570,11 @@ class Game:
             text = fonter.render('GAME OVER', 80)
             w, h = text.get_rect().size
             system.blit(text, (24 + 192 - w / 2, 24 + 192 - h / 2))
-            if score < 5000:
+            if self.score < 5000:
                 msg = 'YUO = TEH L0SER'
-            elif score < 15000:
+            elif self.score < 15000:
                 msg = 'WELL, AT LEAST YOU TRIED'
-            elif score < 30000:
+            elif self.score < 30000:
                 msg = 'W00T! YUO IS TEH R0X0R'
             else:
                 msg = 'ZOMFG!!!111!!! YUO PWND!!!%$#@%@#'
